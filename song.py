24035a0c6fe1d0cf16437564a64b29a75a2d09d7
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-08-15 21:19:54 -0300

    add save func

--- song.py
+++ song.py
@@ -3,6 +3,7 @@ from specutils.spectra import Spectrum1D, SpectralRegion
 from specutils.fitting import fit_continuum,fit_generic_continuum
 from astropy.modeling.models import Polynomial1D,Const1D,Chebyshev1D
 from specutils.manipulation import LinearInterpolatedResampler
+from pyspeckit import Spectrum
 
 def extract_line(file,center,order):
     lines={5015: 17,
@@ -34,4 +35,11 @@ def resample(spectrum):
     resampler = LinearInterpolatedResampler(extrapolation_treatment='zero_fill')
     x = np.linspace(sppectrum.spectral_axis[0].value,spectrum.spectral_axis[-1].value,2048)
     return resampler.resample1d(spectrum,x*u.AA)
+
+def save(spectrum,name):
+    new_spectrum = Spectrum(xarr=spectrum.spectral_axis, data = spectrum.flux,
+                           header=spectrum.header)
+    new_spectrum.write(name+'.fits',type='fits')
+
+
 
