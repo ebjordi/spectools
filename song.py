b2f62a380ff9fe03ef98694da03402daf3ef7aa3
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-08-16 21:02:56 -0300

    add docstring

--- song.py
+++ song.py
@@ -1,36 +1,37 @@
 import astropy.io.fits as pf
 from specutils.spectra import Spectrum1D, SpectralRegion
-from specutils.fitting import fit_continuum,fit_generic_continuum
-from astropy.modeling.models import Polynomial1D,Const1D,Chebyshev1D
+from specutils.fitting import fit_generic_continuum
+from astropy.modeling.models import Chebyshev1D
 from specutils.manipulation import LinearInterpolatedResampler
 from pyspeckit import Spectrum
 
 def extract_line(file,center,order,**kwargs):
-    data = pf.getdata(file) # Get the data
+"Extract line from SONG spectrum and fit fit_continuum. For usage on other
+lines, `exclude_regions` in `fit` "
+data = pf.getdata(file) # Get the data
     header = pf.getheader(file) # Get the full header
     x = data[3,order,:]
     y = data[0,order,:]
     blaze=data[2,order,:]
     line = Spectrum1D(flux=(y/blaze)*u.adu, spectral_axis=x*u.Angstrom, **kwargs)
-    g1_fit = fit_generic_continuum(line,model=Chebyshev1D(4),exclude_regions=[
+    fit = fit_generic_continuum(line,model=Chebyshev1D(4),exclude_regions=[
                                     SpectralRegion(line.spectral_axis[0],line.spectral_axis[100]),
                                     SpectralRegion((line_0-3.)*u.Angstrom,(line_0+4.)*u.Angstrom)
                                     ])
 
-    line = (line / g1_fit(x*u.Angstrom))
+    line = (line / fit(x*u.Angstrom))
     return line, header
 
 
 def resample(spectrum):
     """Return a resampled spectrum with equidistant spectral axis"""
     resampler = LinearInterpolatedResampler(extrapolation_treatment='zero_fill')
-    x = np.linspace(sppectrum.spectral_axis[0].value,spectrum.spectral_axis[-1].value,2048)
+    x = np.linspace(spectrum.spectral_axis[0].value,spectrum.spectral_axis[-1].value,2048)
     return resampler.resample1d(spectrum,x*u.AA)
 
 def save(spectrum,name):
     new_spectrum = Spectrum(xarr=spectrum.spectral_axis, data = spectrum.flux,
                            header=spectrum.header)
     new_spectrum.write(name+'.fits',type='fits')
-
 
 
