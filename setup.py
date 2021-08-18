ca4dd5c003dd86db587740c53cff5e6a12ca2ce1
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-08-18 17:31:41 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spectools',
+      version='0.1',
+      description='HERMES and SONG tools',
+      url='http://github.com/storborg/funniest',
+      author='Jordi Eguren Brown',
+      author_email='jordi.eguren.brown',
+      license='MIT',
+      packages=['spectools'],
+      zip_safe=False)
