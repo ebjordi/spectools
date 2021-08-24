2389852fa5a68b34bc1bb7b355362e86611237b6
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-08-23 22:26:08 -0300

    file

---
+++ tests/test_song.py
@@ -0,0 +1,11 @@
+
+# 
+# def resampled_spectrum(spectrum)
+# 
+#     return equidistant_resample()
+# 
+# def test_equidist_resample(ResampledSpectrum):
+#     expected_delta = (ResampledSpectrum.spectral_axis[-1] -  ResampledSpectrum.spectral_axis[0]) / len(ResampledSpectrum.spectral_axis)
+#     assert expected_delta ==
+#     ResampledSpectrum.spectral_axis[4]-ResampledSpectrum.spectral_axis[3]
+# 
