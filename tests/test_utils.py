76ab1f96b5a0283391cb3854ce6703a49c4f3ec6
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-08-23 22:18:31 -0300

    remove fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,14 @@
 import pytest
 from spectools.utils import *
 import astropy.io.fits as pf
+from specutils import Spectrum1D
+from numpy.testing import assert_array_equal
 
-@pytest.fixture
-def header():
-    filename = 'data/s1_2016-10-12T04-33-07_ext.fits'
+def header(filename):
     return pf.getheader(filename)
 
-def test_get_keyword(header):
-    keyword = get_keyword(header, 'IMFORM')
+def test_get_keyword():
+    filename = 'data/s1_2016-10-12T04-33-07_ext.fits'
+    keyword = get_keyword(header(filename), 'IMFORM')
     assert keyword == 'FITS'
+
