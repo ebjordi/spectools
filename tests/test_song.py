0eacf24954d75e5f57da2586215999bd81e4dc6f
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-08-27 17:48:33 -0300

    refactor: test_equidistant_resample for extendability

--- tests/test_song.py
+++ tests/test_song.py
@@ -8,12 +8,15 @@ def song_spectrum():
     filename = 'data/song_spectrum.fits'
     return extract_line(filename, 24)
 
+@pytest.fixture
+def equidistant_spectrum(song_spectrum):
+    return equidistant_resample(song_spectrum[0])
+
 def test_extract_line(song_spectrum):
     assert len(song_spectrum[0].spectral_axis) == 2048
     #agregar algún otro despues
 
-def test_equidist_resample(song_spectrum):
-    resampled_spectrum = equidistant_resample(song_spectrum[0])
-    delta = resampled_spectrum.spectral_axis.value[1:] - resampled_spectrum.spectral_axis.value[:-1]
+def test_equidistant_resample(equidistant_spectrum):
+    delta = equidistant_spectrum.spectral_axis.value[1:] - equidistant_spectrum.spectral_axis.value[:-1]
     assert_allclose(delta[1:-1],delta[2])
 
