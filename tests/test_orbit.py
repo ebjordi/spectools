33b8ca9c8daebc55fc02ab5cec90131cc4fe649a
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-09-03 00:04:59 -0300

    test: tru anomaly passing

--- tests/test_orbit.py
+++ tests/test_orbit.py
@@ -9,31 +9,20 @@ JD_phase_EA = [
      [57902.4839, 0.75, False],
 ]
 
-phase_EA=[[
+phase_EA_TA=[[
         [0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9],
-        [0.,1.34412783,1.94137176,2.38774944,2.77593815,3.14159265,3.50724716,3.89543586,4.34181355,4.93905748]]]
-@pytest.fixture
-def phase_true_anomaly():
-    phase = []
-    expected_true_anomaly = []
-    with open('data/phase-true-anomaly.dat') as f:
-       f.readline()
-       for line in f:
-            p, e = line.split()[0],line.split()[1]
-            phase.append(float(p))
-            expected_true_anomaly.append(float(e))
-    return phase, expected_true_anomaly
+        [0.,1.34412783,1.94137176,2.38774944,2.77593815,3.14159265,3.50724716,3.89543586,4.34181355,4.93905748],
+        [0.0,2.22772102,2.61851838,2.83435559,2.99720062,3.14159265,3.28598469,3.44882972,3.66466693,4.05546429]]]
 
 @pytest.mark.parametrize("jd,expected_phase,mean_anomaly", JD_phase_EA)
 def test_phase(jd, expected_phase, mean_anomaly):
     assert phase(jd, T0=57880.634,P=29.1333,  mean_anomaly=mean_anomaly) == pytest.approx(expected_phase, 0.005)
 
-@pytest.mark.parametrize("phases,expected_mean_anomaly",phase_EA)
-def test_excentric_anomaly(phases,expected_mean_anomaly):
-    npt.assert_allclose(excentric_anomaly(phases,0.7346),expected_mean_anomaly,rtol=1e-05,atol=1e-06)
+@pytest.mark.parametrize("phases,expected_e_anomaly,t",phase_EA_TA)
+def test_excentric_anomaly(phases,expected_e_anomaly,t):
+    npt.assert_allclose(excentric_anomaly(phases,0.7346),expected_e_anomaly,rtol=1e-05,atol=1e-06)
 
-def test_true_anomaly(phase_true_anomaly):
-    phases,expected_true_anomaly=phase_true_anomaly
-    E = excentric_anomaly(phases, e = 0.73462)
-    true_anomaly_calculated = true_anomaly(E, e = 0.73462)
-    assert true_anomaly_calculated == pytest.approx(expected_true_anomaly,rel=0.1,abs=0.005)
+@pytest.mark.parametrize("ph,expected_EA,expected_TA",phase_EA_TA)
+def test_true_anomaly(ph,expected_EA,expected_TA):
+    true_anomaly_calculated = true_anomaly(expected_EA, e = 0.73462)
+    assert true_anomaly_calculated == pytest.approx(expected_TA,0.005)
