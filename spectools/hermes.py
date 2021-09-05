afbc641c0037827545a453d5bbcd95498e0e829f
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-09-05 14:13:41 -0300

    move orbit_function to orbit

--- spectools/hermes.py
+++ spectools/hermes.py
@@ -1,24 +1,16 @@
 import astropy.io.fits as pf
 from scipy.interpolate import interp1d
 import pandas as pd
+from specutils import Spectrum1D
+from spectools.utils import wavelength
 
 def extract_spectrum(filename, normalized = True):
     """Extract wavelengh and flux for FIES and HERMES spectra"""
     hdu = pf.open(filename)
     data = hdu[0].data
     header = hdu[0].header
-    wave = wavelength(header)
     if normalized:
-        return wave,data[0],header
-    return Spectrum1d(flux=data[1],spectral_axis=data[0]),header
-
-def orbit_function(kepler_file : str):
-    """Given a kepler output file returns interpolated funtions for primary and
-    secondary components of a binary system"""
-    names = ['fase','vr-p','vr-s']
-    df = pd.read_table(kepler_file,names=names,sep='\s+', skiprows=1 ,index_col=False)
-    
-    primary =   interp1d(df["fase"],df["vr-p"], kind='cubic')
-    secondary = interp1d(df["fase"],df["vr-s"], kind='cubic')
-    return primary, secondary
+        wave = wavelength(header)
+        return Spectrum1D(flux=data[0],spectral_axis=wave),header
+    return Spectrum1D(flux=data[1],spectral_axis=data[0]),header
 
