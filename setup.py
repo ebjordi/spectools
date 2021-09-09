6cae8a1b4e4671e037ad32d1cb36b49ee257fd72
Jordi <jordi@fcaglp.unlp.edu.ar>
2021-09-09 19:53:55 -0300

    break: moving orbit to its own package

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from distutils.core import setup
 
 setup(name='spectools',
-      version='0.1',
+      version='0.5',
       description='HERMES and SONG tools',
       url='https://github.com/jordi5/spectools.git',
       author='Jordi Eguren Brown',
       author_email='jordi.eguren.brown@gmail.com',
-      py_modules=['spectools.orbit','spectools.utils','spectools.hermes','spectools.song'])
+      py_modules=['spectools.utils','spectools.hermes','spectools.song'])
