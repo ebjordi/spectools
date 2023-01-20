e69c472e96198082d57b809c87d3f97cb8519360
Jordi Eguren Brown <jordi@fcaglp.unlp.edu.ar>
2023-01-20 01:43:26 -0300

    add util functions

--- src/spectools/utils/utils.py
+++ src/spectools/utils/utils.py
@@ -1,6 +1,8 @@
 import astropy.io.fits as pf
-from numpy import array as nparray
+import astropy.constants as c
+import numpy as np
 from datetime import datetime
+from scipy.interpolate import interp1d
 
 def get_keyword(header,keyword : str):
     """Extracts keyword from header spectra"""
@@ -9,7 +11,7 @@ def get_keyword(header,keyword : str):
 
 def wavelength(header):
     wave = [header['CRVAL1'] + header['CDELT1'] * i for i in range(header['NAXIS1'])]
-    return nparray(wave)
+    return np.array(wave)
 
 
 def add_UT_to_header(spectra):
@@ -33,3 +35,72 @@ def mask_line(x,y,a,b):
     mask = np.ma.getmask(x_ma)
     f_ma=np.ma.array(y,mask=mask)    
     return x_ma,f_ma
+
+def open_file(filename): #for 1Dspecs ?
+    hdu = pf.open(filename)
+    data = hdu[0].data
+    header = hdu[0].header
+    wave = wavelength(header)
+    return wave,data
+
+def ascii_spec(filename):
+    a = np.genfromtxt(filename,delimiter = '  ')
+    w = a[:,0]
+    f = a[:,-1]
+    return w,f
+
+
+def shift(wvl,flux,v,new_wvl = None):
+    cvel =  c.c.to('km/s').value
+    wlprime = wvl * (1.0 + v / cvel)
+    if new_wvl is not None:
+        wvl = new_wvl
+    nflux = interp1d(wlprime, flux, bounds_error=False, fill_value=1)(wvl)
+    return nflux    
+
+def cut_resample(w_model,f_model,w_obj,f_obj,a,b):
+    x_o = np.where((w_obj>a)&(w_obj<b),w_obj,0)
+    y_o = f_obj[x_o>0]
+    x_o = x_o[x_o>0]
+    
+    x_e = np.where((w_model>a)&(w_model<b),w_model,0)
+    y_e = f_model[x_e>0]
+    x_e = x_e[x_e>0]
+    y_e = interp1d(x_e, y_e, bounds_error=False, fill_value=1)(x_o)
+    return x_o,y_o,y_e
+
+
+def get_keyword(file : str,keyword : str):
+    """Extracts keyword from header spectra"""
+    header = pf.getheader(file)
+    return header.get(keyword)
+
+
+#set the templates to 0 km/s
+def displacement_avg(measurements : Union(list, np.array), star):
+    """Calculate redshift velocities and average velocity starting for lines meassured by hand
+    Tailored for Iota Ori A"""
+    lambda_0 = np.array([4025.6, 4713.146, 4921.931, 5015.68, 5411.52, 5592.252])
+    cvel =  c.c.to('km/s').value
+
+    if star == 'secondary':
+        lambda_0 = lambda_0[:-2]
+    vels = cvel * (np.array((measurements) - lambda_0) / lambda_0)
+    return vels,vels.mean()
+
+def displacement(measurement, line, star):
+    """Calculate redshift velocity for lines meassured 
+    Tailored for Iota Ori A"""
+
+    if isinstance(line, int):
+        lambda_0 = lines_dict[str(line)]
+    else:
+        lambda_0 = line
+
+    lines_dict = {4025:4025.6, 4713 : 4713.146, 4921 : 4921.931, 5015 : 5015.68, 5411 : 5411.52, 5592 : 5592.252}
+    c =  299792.458
+
+    if star == 'secondary':
+        lambda_0 = lambda_0[:-2]
+    vels = c * (np.array((measurement) - lambda_0) / lambda_0)
+    avg = vels.mean()
