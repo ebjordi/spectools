7bb11fd9cb3c63e102ee326793c116fc7d267b1a
Jordi Eguren Brown <jordi@fcaglp.unlp.edu.ar>
2023-01-25 16:11:01 -0300

    add module files

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python
-from distutils.core import setup
+from setuptools import find_namespace_packages
+from setuptools import setup
+from glob import glob
+from os.path import splitext
+from os.path import basename
 
 setup(name='spectools',
       version='1.0',
-      description='HERMES and SONG tools',
+      description='package to handle and plot HERMES, SONG and FIES spectra',
       url='https://github.com/ebjordi/spectools.git',
       author='Jordi Eguren Brown',
       author_email='jordi.eguren.brown@gmail.com',
-      py_modules=['spectools.utils','spectools.hermes','spectools.song','spectools.models'])
+      packages=find_namespace_packages("src"),
+      package_dir={"": "src"},
+      py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+      include_package_data=True,
+      zip_safe=False,
+)
